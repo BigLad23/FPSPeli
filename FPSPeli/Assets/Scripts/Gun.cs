fd1f276adb9e88f7ca09a44e3fe3dae7fc79838e
BigLad23 <60403958+BigLad23@users.noreply.github.com>
2021-11-29 14:38:20 +0200

    reload ja muzzle flash lisätty

--- FPSPeli/Assets/Scripts/Gun.cs
+++ FPSPeli/Assets/Scripts/Gun.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -5,12 +6,37 @@ public class Gun : MonoBehaviour
     
     public float damage = 100f;
     public float range = 100f;
+    public int maxAmmo = 30;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
 
+    public ParticleSystem MuzzleFlash;
     public Camera playerCamera;
+    public Animator animator;
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable ()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
     void Update()
     {
+        if (isReloading)
+            return;
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo) // If the player presses R reload begins
+        {
+            Debug.Log("test");
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
@@ -18,6 +44,9 @@ public class Gun : MonoBehaviour
     }
     void Shoot()
     {
+        Debug.Log(currentAmmo);
+        currentAmmo = currentAmmo - 1;
+        MuzzleFlash.Play(); // plays the muzzle flash animation when you fire
         RaycastHit hit;
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
         {
@@ -30,5 +59,16 @@ public class Gun : MonoBehaviour
                 enemy.TakeDamage(damage);
             }
         }
+    }
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+        animator.SetBool("Reloading", true);
+        yield return new WaitForSeconds(reloadTime - .25f);
+        currentAmmo = maxAmmo; // Sets the current ammo to max ammo
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(reloadTime - .25f);
+        isReloading = false;
     }
 }
